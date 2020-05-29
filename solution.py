26af8edc4158890a571c92e6364251294b9bc3b5
Lars Nelson <lnelson7820@gmail.com>
2020-05-29 16:43:23 -0700

    Lars Nelson completed

--- solution.py
+++ solution.py
@@ -1,2 +1,14 @@
 def bracket_matcher(input):
-  pass
+  braces = {'(':')', '[':']', '{':'}'}
+  stacked = []
+  for each in input:
+    if each in braces.keys(): stacked.append(each)
+    if each in braces.values():
+      try: 
+        if braces[stacked.pop()] != each: return False
+      except: return False
+  if len(stacked) != 0: return False
+  else: return True
+
+print(bracket_matcher('(a)'))
+print(bracket_matcher('[a(b)]}'))
