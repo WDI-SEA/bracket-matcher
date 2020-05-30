4448222a39e0e19e9f4247120678aebbb254b2b9
gjones81 <jones.guy81@gmail.com>
2020-05-29 18:15:16 -0700

    Working

--- solution.py
+++ solution.py
@@ -1,2 +1,41 @@
 def bracket_matcher(input):
-  pass
+  close_brackets = [']', ')', '}']
+  open_brackets = ['[', '(', '{']
+  sample_jar = []
+  for i in range(len(input)):
+    if input[i] in open_brackets:
+      sample_jar.append(input[i])
+    elif input[i] in close_brackets:
+      sample = sample_jar.pop()
+      if sample != input[i]:
+        return False
+    else:
+      return True
+
+    
+print('1', bracket_matcher('abc(123)'))
+# returns true
+
+print('2', bracket_matcher('a[b]c(123'))
+# returns false -- missing closing parens
+
+print('3', bracket_matcher('a[bc(123)]'))
+# returns true
+
+print('4', bracket_matcher('a[bc(12]3)'))
+# returns false -- improperly nested
+
+print('5', bracket_matcher('a{b}{c(1[2]3)}'))
+# returns true
+
+print('6', bracket_matcher('a{b}{c(1}[2]3)'))
+# returns false -- improperly nested
+
+print('7', bracket_matcher('()'))
+# returns true
+
+print('8', bracket_matcher('[]]'))
+# returns false - no opening bracket to correspond with last character
+
+print('9', bracket_matcher('abc123yay'))
+# returns true -- no brackets = correctly matched
