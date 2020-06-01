3b5cf2b107d1882940abbf5c994f66a02cb35fd2
ObtuseSpruce <williamllapitan@gmail.com>
2020-06-01 09:25:08 -0700

    fixed hw to just use a single stack

--- solution.py
+++ solution.py
@@ -1,42 +1,56 @@
 def bracket_matcher(input):
+  # openBrackets = {'(', '[', '{'}
+  # closBrackets = {')', ']', '}'}
+  openBrackets = {'(' : ')', '[' : ']', '{' : '}'}
+  closBrackets = {')' : '(', ']' : '[', '}' : '{'}
+  stack = []
+  for c in input:
+    if c in openBrackets: stack.append(c)
+    elif c in closBrackets:
+      if not stack: return False
+      if not openBrackets[stack.pop()] == c: return False
+  return not len(stack)
 
-  openBrackets = {'(', '[', '{'}
-  closBrackets = {')', ']', '}'}
-  checkOpen = []
-  checkClosed = []
+  # checkOpen = []
+  # checkClosed = []
 
-  for i in input:
-    if i in openBrackets:
-      checkOpen.append(i)
-    if i in closBrackets:
-      if i == ')':
-        checkClosed.insert(0, '(')
-      elif i == ']':
-        checkClosed.insert(0, '[')
-      elif i == '}':
-        checkClosed.insert(0, '{')
-  # print(checkOpen)
-  # print(checkClosed)
+  # for i in input:
+  #   if i in openBrackets:
+  #     checkOpen.append(i)
+  #   if i in closBrackets:
+  #     if i == ')':
+  #       checkClosed.insert(0, '(')
+  #     elif i == ']':
+  #       checkClosed.insert(0, '[')
+  #     elif i == '}':
+  #       checkClosed.insert(0, '{')
+  # # print(checkOpen)
+  # # print(checkClosed)
 
-  if len(checkClosed) == 0 and len(checkOpen) == 0:
-    return True
-  if len(checkClosed) != len(checkOpen):
-    return False
+  # if len(checkClosed) == 0 and len(checkOpen) == 0:
+  #   return True
+  # if len(checkClosed) != len(checkOpen):
+  #   return False
 
-  for i in range(len(checkOpen)):
-      if checkOpen[i] != checkClosed[i]:
-        return False
-      else:
-        return True
+  # for i in range(len(checkOpen)):
+  #     if checkOpen[i] != checkClosed[i]:
+  #       return False
+  #     else:
+  #       return True
+
+
+
+
+  
 
 # print(bracket_matcher('{(bxbxa)(bxbxb)}'))
 # print(bracket_matcher('[dasdy(c)ddd  ] alkd }'))
 
-# print(bracket_matcher('abc(123)'))
+print(bracket_matcher('abc(123)'))
 # returns true
 
-# print(bracket_matcher('a[b]c(123'))
-# # returns false -- missing closing parens
+print(bracket_matcher('a[b]c(123'))
+# returns false -- missing closing parens
 
 # print(bracket_matcher('a[bc(123)]'))
 # # returns true
