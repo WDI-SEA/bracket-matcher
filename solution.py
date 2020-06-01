008552026f84897879badb642e71bfcfc3016cd4
samuelhhawkins <samuel.h.hawkins@hotmail.com>
2020-05-31 22:59:52 -0700

    created bracket tracker had problems catching single ending brackets

--- solution.py
+++ solution.py
@@ -1,2 +1,39 @@
 def bracket_matcher(input):
-  pass
+    catch = []
+    brackets = {'[':']', '{':'}', '(':')'}
+    for i in input:
+        if i in brackets.keys(): catch.append(i)
+        if i in brackets.values():
+            # if not brackets:
+            #     return False
+            if brackets[catch.pop()] != i: return False
+
+    if len(catch) != 0: return False
+    else: return True
+
+print(bracket_matcher('abc(123)'))
+# returns true
+
+print(bracket_matcher('a[b]c(123'))
+# # returns false -- missing closing parens
+#
+print(bracket_matcher('a[bc(123)]'))
+# # returns true
+#
+print(bracket_matcher('a[bc(12]3)'))
+# # returns false -- improperly nested
+#
+print(bracket_matcher('a{b}{c(1[2]3)}'))
+# # returns true
+#
+print(bracket_matcher('a{b}{c(1}[2]3)))'))
+# # returns false -- improperly nested
+#
+print(bracket_matcher('())'))
+# # returns true
+#
+print(bracket_matcher('[]'))
+# # returns false - no opening bracket to correspond with last character
+#
+print(bracket_matcher('abc123yay'))
+# # returns true -- no brackets = correctly matched
