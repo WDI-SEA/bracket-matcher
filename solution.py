9c2d52c321aa79be38832f9b9fd833d225d6dd21
davidvelichko52 <davidvelichko52@gmail.com>
2020-06-01 09:07:16 -0700

    hw

--- solution.py
+++ solution.py
@@ -1,2 +1,26 @@
-def bracket_matcher(input):
-  pass
+
+  
+
+def bracket_matcher(input): 
+  open_list = ["[","{","("] 
+  close_list = ["]","}",")"] 
+  stack = [] 
+
+  for i in input: 
+        if i in open_list: 
+            stack.append(i) 
+        elif i in close_list: 
+            pos = close_list.index(i) 
+            if ((len(stack) > 0) and
+                (open_list[pos] == stack[len(stack)-1])): 
+                stack.pop() 
+            else: 
+                return "false"
+  if len(stack) == 0: 
+        return "true"
+  else: 
+        return "false"
+
+
+
+print(bracket_matcher("({[})")) 
