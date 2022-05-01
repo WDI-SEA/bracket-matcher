559e593e67522928b37791409ab9962e815ef2f2
weston-bailey <wdbailey87@gmail.com>
2022-05-01 10:29:48 -0700

    psudocode

--- solution.py
+++ solution.py
@@ -1,2 +1,12 @@
+
+
 def bracket_matcher(input):
-  pass
+    # stack to keep track of brackets
+    # need to know all valid characters
+
+    # loop over input and check if char is a valid character
+        # if char is a valid character check if it is opening
+            # if it is opening, add to stack
+            # else if it is closing check, make sure it has a matching closing first in the stack
+                # if so, pop it off the stack and keep going 
+                # if not, return false, because it is a mismatch
