4d6379e74a2eb55376473d5d4b590dd7f7f9a42e
weston-bailey <wdbailey87@gmail.com>
2022-07-06 14:46:42 -0700

    uses back of list

--- solution.py
+++ solution.py
@@ -14,13 +14,15 @@ def bracket_matcher(input):
     for char in input:
         # if it is opening, add to stack
         if char in opening_chars:
-            brackets.insert(0, char)
+            # brackets.insert(0, char)
+            brackets.append(char)
         # else if it is closing check, make sure it has a matching closing first in the stack
         elif char in closing_chars:
             if len(brackets) > 0:
                 if brackets[0] == closing_chars[char]:
                     # if so, pop it off the stack and keep going 
-                    brackets = brackets[1:]
+                    # brackets = brackets[1:]
+                    brackets.pop()
                 else:
                     # if not, return false, because it is a mismatch
                     return False
