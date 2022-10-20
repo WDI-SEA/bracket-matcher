29df6fe85b89d63cb2d53ee03d3f107d82b371af
Manion-sebastian <73517798+Manion-sebastian@users.noreply.github.com>
2022-10-20 16:51:57 -0700

    completed SM

--- solution.py
+++ solution.py
@@ -1,2 +1,26 @@
-def bracket_matcher(input):
-  pass
+def isValid(nput):
+    if len(nput) % 2 != 0:
+        return False
+    checked_string = ''
+    dict = {'(' : ')', '[' : ']', '{' : '}'}
+    check = []
+    for i in nput:
+        if i in dict.keys() or i in dict.values():
+            checked_string += i
+    if len(checked_string) > 0:
+        for i in checked_string:
+            if i in dict:
+                check.append(i)
+            else:
+                if check == []:
+                    return False
+                a = check.pop()
+                if i != dict[a]:
+                    return False
+        return check == []
+    else:
+        return False
+
+print(isValid("(ad)[fas]a[asda]"))
+print(isValid("()][]"))
+print(isValid(""))
