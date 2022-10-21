25de232cadda5bd6c78f9b5e9286e02f9d065ef3
elek8871 <elekmichelle@gmail.com>
2022-10-21 09:58:33 -0600

    adds progess

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 
-open = ["{", "[", "("]
-closed = [ "}", "]", ")"]
+open_bracket = ["{", "[", "("]
+close_bracket= [ "}", "]", ")"]
 
 def bracket_matcher(input):
     # set empty stack
@@ -8,13 +8,21 @@ def bracket_matcher(input):
     # iterate through the array to search for open brackets
     for i in input:
         # if open is in the array move it to the stack
-        if i in open:
-            stack.append(i)
+        if i in open_bracket:
+            stack.append[i]
+            print stack[i]
         # if not save the index so you can compare to in the stack
-        elif i in closed:
-            closed[i] 
+        elif i in close_bracket:
+            closed = close_bracket[i] 
         # if the closed[i] is in stack then pop open from stack
+    if closed == stack[i]:
+        stack.pop[i]
+        return True
         # if its not then leave open in stack
+    else:
+        return False
+     # if stack is empty - brackets are all matched and return true
 
-    # if stack is empty - brackets are all matched and return true
     # if stack is not empty - there are unmatched brackets return false
+
+    bracket_matcher('[]]')
