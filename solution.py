c8c0a37e13b5b77685d7d9056e7c572602115839
average-alien <allentang113@gmail.com>
2022-10-21 09:20:21 -0700

    optimized bracket matcher a bit

--- solution.py
+++ solution.py
@@ -40,32 +40,67 @@ def bracket_matcher(input):
 		# all brackets were matched! we can return true
 		return print(True)
 
+def optimized_bracket_matcher(input):
+	# brackets reference
+	opening_brackets = ["[", "{", "("]
+	closing_brackets = {
+		"]": "[",
+		"}": "{",
+		")": "("
+	}
+	# stack for keeping track of brackets in input
+	bracket_stack = []
+	# loop over characters in input
+	for char in input:
+		# if a closing bracket
+		if char in closing_brackets:
+			# if stack is empty
+			if len(bracket_stack) == 0:
+				# we can return false (this bracket has no opening)
+				return print(False)
+			# else if top of stack matches opening bracket for current closing bracket
+			elif bracket_stack[-1] == closing_brackets[char]:
+				# pop off top of stack
+				bracket_stack.pop()
+			# else we can return false (top of stack does not match current closing bracket)
+			else: return print(False)
+		# if opening bracket
+		elif char in opening_brackets:
+			# push to stack
+			bracket_stack.append(char)
+	
+	# if we make it down here and the stack is empty
+	if len(bracket_stack) == 0:
+		# return true (all our brackets were matched!)
+		return print(True)
+	# else return false (a bracket was never closed)
+	else: return print(False)
+
+
 # test cases #
-bracket_matcher('abc(123)')
+optimized_bracket_matcher('abc(123)')
 # returns true
 
-bracket_matcher('a[b]c(123')
+optimized_bracket_matcher('a[b]c(123')
 # returns false -- missing closing parens
 
-bracket_matcher('a[bc(123)]')
+optimized_bracket_matcher('a[bc(123)]')
 # returns true
 
-bracket_matcher('a[bc(12]3)')
+optimized_bracket_matcher('a[bc(12]3)')
 # returns false -- improperly nested
 
-bracket_matcher('a{b}{c(1[2]3)}')
+optimized_bracket_matcher('a{b}{c(1[2]3)}')
 # returns true
 
-bracket_matcher('a{b}{c(1}[2]3)')
+optimized_bracket_matcher('a{b}{c(1}[2]3)')
 # returns false -- improperly nested
 
-bracket_matcher('()')
+optimized_bracket_matcher('()')
 # returns true
 
-bracket_matcher('[]]')
+optimized_bracket_matcher('[]]')
 # returns false - no opening bracket to correspond with last character
 
-bracket_matcher('abc123yay')
+optimized_bracket_matcher('abc123yay')
 # returns true -- no brackets = correctly matched
-
-bracket_matcher('}')
