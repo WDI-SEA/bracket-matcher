5f70321c4ae7e26f4fcaaf115007fc28f7a1d764
devin-lynch <devin.l.lynch@gmail.com>
2022-10-21 11:18:30 -0500

    Review pseudocode

--- solution.py
+++ solution.py
@@ -1,3 +1,31 @@
+# ## # ## # ## # ## #
+# CLASS REVIEW
+
+def bracket_matcher(input):
+  # use a list to hold opening character to determine if a new pair has started
+  # use a dictionary to establish the pairs
+  # use an empty list variable to store the opening brackets for later comparisons
+
+  # loop over all of input
+  # if the current element is a bracket (check our list) then push the bracket to our stack variable for later comparisons
+
+  # if the current element is a closing bracket (check our dictionary)
+    # if the stack variable has no length, then the closing bracket current element will never have a match, so return False!
+    # if the last element in the stack variable is exactly equal to the value associated with the key that is the current element, then return True!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # class Node:
 #   def __init__(self, data):
 #     self.data = data
@@ -53,18 +81,26 @@
 
 
 
-def bracket_matcher(input):
-  valid_chars = ''
+# def bracket_matcher(input):
+#   valid_chars = ''
 
-  table = []
-  object = { '(' : ')', '[' : ']', '{' : '}' }
-  for i in input:
-    if i in object.keys() or i in object.values():
-      valid_chars += i
-      print(valid_chars)
-      # my_stack.push(i)
+#   table = []
+#   object = { '(' : ')', '[' : ']', '{' : '}' }
+#   for i in input:
+#     if i in object.keys() or i in object.values():
+#       valid_chars += i
+#       print(valid_chars)
+#   if len(valid_chars) != 0:
+#     for i in valid_chars:
+#       if i in object:
+#         table.append(i)
+#       else:
+#         if table == []:
+#           return False
+#         j = table.pop()
+#       # my_stack.push(i)
 
-  # print(my_stack)
+#   # print(my_stack)
 
 
 
