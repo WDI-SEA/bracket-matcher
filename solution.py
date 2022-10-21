60b6c3bf15eb6cbd605f34fb6970c120ad92184d
aimeepham14 <aimee.pham14@gmail.com>
2022-10-21 09:05:04 -0700

    bracket matcher attempt

--- solution.py
+++ solution.py
@@ -1,2 +1,27 @@
 def bracket_matcher(input):
-  pass
+  stack = []
+ 
+  for char in input:
+      if char in ["(", "{", "["]:
+ 
+        # Push the element in the stack
+        stack.append(char)
+      else:
+ 
+      # IF current character is not opening
+      # bracket, then it must be closing.
+      # So stack cannot be empty at this point.
+        if not stack:
+          return False
+          current_char = stack.pop()
+        if current_char == '(':
+        if char != ")":
+          return False
+        if current_char == '{':
+        if char != "}":
+          return False
+        if current_char == '[':
+        if char != "]":
+          return False
+
+ 
