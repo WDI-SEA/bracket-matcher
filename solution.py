d9a2979237d6d55e4292192bb61acf106ca86ae2
tatebonham <tatebonham@gmail.com>
2022-10-21 09:37:16 -0700

    Finished Deliverable, Fixed

--- solution.py
+++ solution.py
@@ -1,2 +1,23 @@
 def bracket_matcher(input):
-  pass
+    open_brackets = ["(", "[", "{"]
+    close_brackets = {
+        ")": "(",
+        "]": "[",
+        "}": "{"
+    }
+    bracket_stack = []
+    for bracket in input:
+        print("bracket: ", bracket)
+        if bracket in open_brackets:
+            bracket_stack.append(bracket)
+        if bracket in close_brackets:
+            if len(bracket_stack) < 1:
+                return False
+            if close_brackets[bracket] == bracket_stack[len(bracket_stack)-1]:
+                bracket_stack.pop()
+    if len(bracket_stack) > 0:
+        return False
+    return True
+
+
+print(bracket_matcher('abc(123)'))
