7c4c35248b92700f94faceefa35cc1c8e9e8fb88
prijacash <prijacash@gmail.com>
2022-10-20 19:02:01 -0700

    updated completed deliverable

--- solution.py
+++ solution.py
@@ -1,2 +1,27 @@
 def bracket_matcher(input):
-  pass
+    if len(nput) % 2 !=0:
+        return False
+    checked_string = ''
+    
+    dict = {
+            "{" : "}",
+            "[" : "]",
+            "(" : ")"
+        }
+    check = []
+
+    for i in nput:
+        if i in dict.keys() or i in dict.values():
+            checked_string += i
+    if len(checked_string) > 0:
+      for i in checked_string:
+        if i in dict:
+            check.append(i)
+        else:
+            if check == []:
+                return False
+            a = check.pop()
+            if i != dict[a]:
+                return False
+        return check == []
+properBrackets('(sdalfkjalskf)')
