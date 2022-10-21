f6b1bcdd97531c1fe7a6c0bec0a8539809b820a0
Shazammon <ammonorgill@gmail.com>
2022-10-21 11:05:57 -0600

    updates

--- solution.py
+++ solution.py
@@ -1,3 +1,19 @@
 def bracket_matcher(input):
   # list to hold opening 
-  pass
+  for bracket in input:
+    print("BRACKET\n", bracket)
+    if bracket in open_brackets:
+      bracket_stack.append(bracket)
+      print("STACK\n", bracket)
+      if bracket in open_bracket:
+          bracket_stack.append(bracket)
+          print("STACK\n", bracket)
+      if bracket in close_brackets:
+        if len(bracket_stack) < 1
+  if close)brackets[bracket] == bracket_stack[len(bracket_stack) - 1]
+  if len(bracket_stack) > 0:
+    return False
+  
+  return True
+
+  print("True")
